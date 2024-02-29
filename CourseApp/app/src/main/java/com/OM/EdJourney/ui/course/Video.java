a06398cdfddd6c5391ef7b8e5b268dcc4d8134cb
MarwanEA <marwanelaoumrani@gmail.com>
2024-02-29 03:47:48 +0100

    .

--- CourseApp/app/src/main/java/com/OM/EdJourney/ui/course/Video.java
+++ CourseApp/app/src/main/java/com/OM/EdJourney/ui/course/Video.java
@@ -12,6 +12,7 @@ import com.OM.EdJourney.model.Chapter;
 import com.OM.EdJourney.retrofit.ApiInterface;
 import com.OM.EdJourney.retrofit.RetrofitClient;
 import com.OM.EdJourney.ui.chapter.CourseChaptersList;
+import com.OM.EdJourney.ui.quiz.PreQuizActivity;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
@@ -27,7 +28,8 @@ import retrofit2.Response;
 
 public class Video extends AppCompatActivity {
     ApiInterface apiInterface;
-     YouTubePlayerView youTubePlayerView;
+    YouTubePlayerView youTubePlayerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,8 +57,9 @@ public class Video extends AppCompatActivity {
             public void onResponse(Call<Chapter> call, Response<Chapter> response) {
                 Chapter chapter = response.body();
                 // Assuming you want a vertical list, use LinearLayoutManager
-                 setVideoID(chapter);
+                setVideoID(chapter);
             }
+
             @Override
             public void onFailure(Call<Chapter> call, Throwable t) {
                 Toast.makeText(Video.this, "Failed to fetch chapters", Toast.LENGTH_SHORT).show();
@@ -81,7 +84,21 @@ public class Video extends AppCompatActivity {
                 // Handle player state changes if needed
             }
         });
+
+
+        // handle click on to_quiz
+        findViewById(R.id.to_quiz).setOnClickListener(view -> {
+            Intent intent1 = new Intent(Video.this, PreQuizActivity.class);
+            intent1.putExtra("chapterName", intent.getStringExtra("chapterName"));
+            intent1.putExtra("chapterId", intent.getLongExtra("chapterId", 0));
+            intent1.putExtra("contentNumber", intent.getIntExtra("contentNumber", 0));
+            intent1.putExtra("courseId", intent.getLongExtra("courseId", 0));
+            intent1.putExtra("courseName", intent.getStringExtra("courseName"));
+
+            startActivity(intent1);
+        });
     }
+
     private void setVideoID(Chapter chapter) {
         if (youTubePlayerView != null) {
             youTubePlayerView.getYouTubePlayerWhenReady(youTubePlayer -> {
